71fe59d287bff52df22b6a41b6b564439202ea80
Erick Alves <britoerick16@gmail.com>
2026-04-03 11:07:32 -0300

    feat: implement global exception filter

---
+++ src/Backend/MyRecipeBook.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,37 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is MyRecipeBookException)
+            HandleProjectException(context);
+        else
+            ThrowUnknowException(context);
+    }
+
+    public static void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            var exception = context.Exception as ErrorOnValidationException;
+            
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            context.Result = new BadRequestObjectResult(new ResponseErrorJson(exception.ErrorMessages));
+        }
+    }
+
+    public static void ThrowUnknowException(ExceptionContext context)
+    {
+        Console.WriteLine(context.Exception.Message);
+        context.HttpContext.Response.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesException.UNKNOW_ERROR));
+    }
+}
