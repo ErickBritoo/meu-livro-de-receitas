c00fa70864327c7207498b698728e20b027ff309
Erick Alves <britoerick16@gmail.com>
2026-04-30 11:20:36 -0300

    git: adding login user use case

--- src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -4,13 +4,14 @@ using MapsterMapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Application.UseCase.Login.DoLogin;
 using MyRecipeBook.Application.UseCase.User.Register;
 
 namespace MyRecipeBook.Application;
 
 public static class DependencyInjectionExtension
 {
-    public static void AddAplication(this IServiceCollection services, IConfiguration configuration)
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
         AddUsesCases(services);
         AddMapster(services);
@@ -20,6 +21,7 @@ public static class DependencyInjectionExtension
     private static void AddUsesCases(IServiceCollection services)
     {
         services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+        services.AddScoped<IDoLoginUseCase, DoLoginUseCase>();
     }
 
     private static void AddMapster(IServiceCollection services)
@@ -35,7 +37,7 @@ public static class DependencyInjectionExtension
     private static void AddPasswordEncripter(IServiceCollection services, IConfiguration configuration)
     {
         var additionalKey = configuration["Settings:Password:AdditionalKey"]!;
-
+        
         services.AddScoped(option => new PasswordEncripter(additionalKey));
     }
 
---
+++ src/Backend/MyRecipeBook.Application/UseCase/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,32 @@
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCase.Login.DoLogin;
+
+public class DoLoginUseCase : IDoLoginUseCase
+{
+    private readonly IUserReadOnlyRepository _repository;
+    private readonly PasswordEncripter _passwordEncripter;
+
+    public DoLoginUseCase(IUserReadOnlyRepository repository, PasswordEncripter passwordEncripter)
+    {
+        _repository = repository;
+        _passwordEncripter = passwordEncripter;
+    }
+    
+    public async Task<ResponseRegisterUserJson> Execute(RequestLoginJson request)
+    {
+        var encriptedPassword = _passwordEncripter.Encrypt(request.Password);
+
+        var user = await _repository.GetByEmailAndPassword(request.Email, encriptedPassword) ??
+                   throw new InvalidLoginException();
+        
+        return new ResponseRegisterUserJson
+        {
+            Name = user.Name
+        };
+    }
+}
